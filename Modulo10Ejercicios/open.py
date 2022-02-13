8a0435a28ed11fa542f9db5a2fa4b11c4bbadcac
Quirozdev <luisdevv232@gmail.com>
2022-02-13 14:38:06 -0700

    Subiendo los ejercicios de la kata 10

---
+++ Modulo10Ejercicios/open.py
@@ -0,0 +1,10 @@
+# open.py
+def main():
+    try:
+        open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main()
